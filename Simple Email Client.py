200299f046d5689ba964e5ba73f3b8319646eed8
gianghht <gianghht.learning@gmail.com>
2017-06-14 15:20:50 +0900

    Finish Send Email by Gmail

--- Simple Email Client.py
+++ Simple Email Client.py
@@ -1,24 +1,49 @@
 ###Simple Email Client###
 #Import#
 import smtplib
+import getpass
 
-#Your email#
-m_email = input("Please input the Senders Email:\n")
-m_server = input("Please input the mail server (For example: 'mail.google.com'):\n")
+#Base#
+send = "True"
+ss = "True"
+repeats = 1
+while send == "True":
+    #Your email#
+    m_email = input("Please input the your email:\n")
+    m_pwd = getpass.getpass("Please input your password:\n")
+    m_server = "smtp.gmail.com"
+    m_port = 587
+    
+    #Recepient email#
+    r_emails = input("Please input a list of comma seperated emails:\n")
+    r_emails = r_emails.replace(" ","").split(",")
 
-#Recepient email#
-r_emails = input("Please input a list of comma seperated emails:\n")
-r_emails = r_emails.replace(" ","").split(",")
+    #Message#
+    subject = input("Input your message subject:\n")
+    subject = "Subject: " + subject
+    main_body = input("Input your main message (New Line = '\\n'):\n")
 
-#Message#
-subject = input("Input your message subject:\n")
-subject = "Subject: " + subject
-main_body = input("Input your main message (New Line = '\\n'):\n")
+    message = subject + "\n \n" + main_body
 
-message = subject + "\n \n" + main_body
+    #Sending the Email#
+    while ss == "True" or repeats == 5:
+        try:
+            smtpObj = smtplib.SMTP(m_server,m_port)
+            smtpObj.ehlo()
+            smtpObj.starttls()
+            smtpObj.login(m_email, m_pwd)
+            smtpObj.sendmail(m_email, r_emails, message)
+            smtpObj.close()
+            print("Mail Sent Successfully!")
+            ss = "False"
+        except:
+            print("Sending Failed!")
+            repeats = repeats + 1
 
-#Test Print#
-print("Sender: " + m_email)
-print("Server: " + m_server)
-print("Recipient(s): "r_emails)
-print("Message: " + message)
+        if input("Send another Email?\n") == "yes":
+            send = "True"
+            ss = "True"
+            repeats = 1
+        else:
+              send = "False"
+              ss = "False"
